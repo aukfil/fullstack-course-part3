3329ebe3a369ab217b1f3817095b44bcc887183f
auksztol <bigiottini@gmail.com>
2024-08-18 12:41:46 +0200

    Add solution to 3.11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('post-body', (request, response) => {
   if (request.method === 'POST') {
