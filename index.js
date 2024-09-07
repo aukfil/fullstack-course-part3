20923fe00ac9964b23476094fc1a110a79705e41
auksztol <bigiottini@gmail.com>
2024-09-07 18:00:35 +0200

    Update phonebook backend

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-morgan.token('post-body', (request, response) => {
-  if (request.method === 'POST') {
-    return JSON.stringify(request.body)
-  }
-  return ''
+// morgan.token('post-body', (request, response) => {
+//   if (request.method === 'POST') {
+//     return JSON.stringify(request.body)
+//   }
+//   return ''
+// })
+
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
+
+// let persons = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
 })
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+const Person = mongoose.model('Person', personSchema)
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
 app.get('/api/persons/:id', (request, response) => {
