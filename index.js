5a73ea655be53c274b5ea37701afaaabfb285690
auksztol <bigiottini@gmail.com>
2024-09-18 18:48:34 +0200

    Add solution to 3.15

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post('/api/persons', (request,response) => {
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
-      response.status(204).end()
+      response.json(result)
     })
     .catch(error => next(error))
 })
